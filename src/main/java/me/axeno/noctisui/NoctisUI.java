49508346d0d55db1b3970ca1aae22641b83fcd88
axenodev <tbavart@gmail.com>
2026-05-30 23:36:26 +0200

    Initialize NoctisUIClient and register NoctisUIConfig in the NoctisUI constructor

--- src/main/java/me/axeno/noctisui/NoctisUI.java
+++ src/main/java/me/axeno/noctisui/NoctisUI.java
@@ -1,6 +1,9 @@
 package me.axeno.noctisui;
 
 import lombok.Getter;
+import me.axeno.noctisui.client.NoctisUIClient;
+import me.axeno.noctisui.config.NoctisUIConfig;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,16 +13,15 @@ public class NoctisUI {
 
     public static final String MODID = "noctisui";
     @Getter private static NoctisUI instance;
-//    private final TestHUD testHUD;
-//    private boolean hudVisible = false;
 
     public NoctisUI() {
         instance = this;
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-//        testHUD = new TestHUD("Mon HUD personnalisé");
+        NoctisUIConfig.register();
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void clientSetup(FMLClientSetupEvent event) {
-
+        NoctisUIClient.init();
     }
 }
